bca6ba16d21475eea14843832a3d5a89ff592db7
tawnchan <tawnchan@gmail.com>
2022-10-10 23:31:34 -0700

    revert main branch back to original file state

--- interview-questions.md
+++ interview-questions.md
@@ -1,125 +1,43 @@
-# ASSESSMENT 5: Interview Practice Questions
-
 Answer the following questions.
 
 First, without external resources. Challenge yourself to answer from memory.
 
 Then, research the question to expand on your answer. Even if you feel you have answered the question completely on your own, there is always something more to learn. Write your researched answer in your OWN WORDS.
 
-1. What is a gem?
-
+What is a gem?
 Your answer:
-    Gems are ruby packages/package manager that contains ruby programs and libraries. They're used to install and contain data.
 
 Researched answer:
-    Ruby gems are not magic. They are open source libraries that contains Ruby programs and libraries and allow programmers to access and use them in their own projects without explicitly inserting those codes.
 
-2. What does the acronym CRUD stand for?
-
+What does the acronym CRUD stand for?
 Your answer:
-    Create, Read, Update, Delete. 
 
 Researched answer:
-    # HOW THE INTERNET WORKS
-    Full-Stack development edition.
 
-Client - Server (Request <=> Response) Cycle.
-
-Request -
-    1) Location
-       *) Where the server is located.
-       - ip address = location address for computers
-       - url = location address for humans
-
-    2) ACTION (CRUD, create, read, update, delete)
-       - Display(read) Content
-       - Create Content
-       - Edit(update) Content
-       - Delete Content
-
-(ACTION) _____HTTP requests/HTTP Verb -
-
-    - Agreed upon protocol of requesting content.
-      - get request
-        - read content
-      - post request
-        - create content
-      - put request
-        - update, replace entire entry
-      - delete request
-        - delete
-      - patch request
-        - update whatever is needed/required
-
-Response -
-    1) Status Code - example: 404 error message
-       1) levels
-            100 - informational
-            200, - all good
-            300 - redirects
-            400, - client side error
-            500 - server side error, response error
-
-    2) Payload
-       1) Always wrapped in HTML
-          1) markup intended for what the user see (HTML, CSS, etc.)
-          2) JSON - Javascript Object Notation
-             1) data
-                1) if you are getting (JSON)DATA you made a request to an API (Application Programming Interface)
-
-3. What is Ruby on Rails?
-
+What is Ruby on Rails?
 Your answer:
-    Ruby on Rails is a gem that is a framework for Ruby to create dynamic full-stack content similar to react is for javascript. 
 
 Researched answer:
-    Ruby on Rails is a web-application framework that includes everything needed to create a database-backed web application. 
 
-4. What is a relational database? Are there other kinds of databases?
-
+What is a relational database? Are there other kinds of databases?
 Your answer:
-    Relational databases are databases where data stored is shared and related across multiple tables. 
-
-    Another kind of database is non-relational databases.  It is generally easier to maintain relational databases than non relational databases.
-    
-Researched answer:
-    Relational databases are structured and modeled in a way that shows relationships between the data. This is especially helpful when an application is scaled so files can be easily traced. Each table is connected to at least one other table and so information is part of a defined structure. 
-
-    Three popular SQL databases are 'PostgreSQL', 'SQLite', and 'MySQL'.
-
-5. What are primary keys? Why are they important?
-
-Your answer:
-    By default databases creates primary keys that will use integers. When we create tables and instances, they will be assigned a primary key.. a kind of unique identifier.
-
 
 Researched answer:
-    The primary key contains unique values and identifies each row.  It cannot contain NULL values. A table can only have one primary key and this primary key can consist of a single or multiple columns.
 
-## Looking Ahead: Terms for Next Week
+What are primary keys? Why are they important?
+Your answer:
 
+Researched answer:
+
+Looking Ahead: Terms for Next Week
 Research and define the following terms to the best of your ability.
 
-1. RESTful routes: 
-    REST is an architectural style(Wikipedia) that is used in Rails by default. RESTful design helps to create default routes by using resources keyword followed by the controller name in the the routes.rb file.
-   
-    For a route to be completely Restful it must do the following:
+RESTful routes:
 
-    Separate the client from the server.
-    Not hold state between requests (i.e. all information necessary to respond to a request is available in each request: no data or state, is held by the server from request to request).
-    Use HTTP and HTTP methods.
-    Be reliable (for e.g most APIs follow the restful route pattern when specifying the process for authentication and important URLs).
+JSON:
 
-    There are 7 different restful routes: INDEX, NEW, CREATE, SHOW, EDIT, UPDATE, DESTROY.
+ERB:
 
-2. JSON:
-    JSON stands for Javascript Object Notation. It is a lightweight format for storing and transporting data in web applications. It is used for describing and exchanging text-based data. 
+Params:
 
-3. ERB:
-    ERB is a Ruby templating language which stands for Embedded Ruby. It allows users to generate any kind of text, in any quantity from templates.
-
-4. Params: 
-    Params stands for parameter methods. they are variables that are defined in the method and can accept arguments. for example, a method within a method where the parameter is a method in the method. Its the information getting passed to a controller via GET or POST requests.
-
-5. API:
-    Application Programming Interface. They're applications with distinct functions usually providing interface and contact communication between two applications. For example... if we have a mapping application we can utilize a geo location service as an api to retrieve geo location data for our app. 
+API:
--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -3,20 +3,6 @@
 
 # --------------------1) Create a method that takes in an array of words and a single letter and returns an array of all the words containing that particular letter. Use the test variables provided.
 
-# INPUT: (Array(words), letter)
-# OUTPUT: [Words containing the particular letter]
-
-# We are going to need to arguments passed in, the array of words and also the letter to be filtered.
-# We are going to have use a iteration method that will iterate through each word in the array.
-# For this we are going to use the .select method that will iterate through the given array and we can pass in another method .include? to check to see if any of the words in the array contain the given letter.
-
-# define the method, named it particular_word and pass in two arguments (array, letter)
-def particular_word(array, letter);
-    # From the array we are going to .select the word values of all the indexes of the array and with that word we use .include?(letter) to check to see if that word contains that letter.
-    array.select { |word| word.include?(letter) }
-    # Since Ruby has an implicit return, we don't need to return anything as the method .select will return an array automatically.
-end
-
 beverages_array = ['coffee', 'tea', 'juice', 'water', 'soda water']
 
 letter_o = 'o'
@@ -24,98 +10,23 @@ letter_o = 'o'
 letter_t = 't'
 # Expected output: ['tea', 'water', 'soda water']
 
-particular_word(beverages_array, letter_o)
-# ["coffee", "soda water"]
-
-particular_word(beverages_array, letter_t)
-# ["tea", "water", "soda water"]
-
 
 # -------------------2) Create a method that takes in a hash and returns one array with all the hash values at their own index and in alphabetical order. No nested arrays. Use the test variable provided.
 # HINT: Google 'ruby get rid of nested arrays'
 
-# INPUT: hash: us_states
-# OUTPUT: Array with no nested arrays, so remove nested arrays and combine the values and alphabetize them.
-
-# So since we know what the idea is.. we have to first figure out how to remove the nested arrays.
-# I browsed and there is a method called .flatten that 'flattens' out nested arrays and returns an array with all the values in just that array. Basically it removes the square brackets and comebines the values into one.
-# After .flatten we can use .sort() which will automatically sort the values alphabetically. 
-
-# I'll define the method alphabetgized_array and it takes in a hash.
-def alphabetized_array(hash);
-    # We have to specifically indicate we want to  flatten the values of the hash, and after the flatten method, can tag on the sort() method to alphabetize the output array.
-    hash.values.flatten.sort()
-end
-
 us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['California', 'Arizona', 'Nevada'], notheast: ['Maine', 'New Hampshire', 'Vermont'] }
 # Expected output: ['Arizona', 'California', 'Idaho', 'Maine', 'Nevada', 'New Hampshire', 'Oregon', 'Vermont', 'Washington'] 
-
-p alphabetized_array(us_states)
-# ["Arizona", "California", "Idaho", "Maine", "Nevada", "New Hampshire", "Oregon", "Vermont", "Washington"]
 
 
 # --------------------3a) Create a class called Bike that is initialized with a model, wheels, and current_speed. The default number of wheels is 2. The current_speed should start at 0. Create a bike_info method that returns a sentence with all the data from the bike object.
 
-# Okay... so if I am understanding this challenge correctly..
-# It it asking me to make a class called Bike that initializes new instances of itself with 3 specific variables: model, wheels: 2, and current speed = 0 mph.
-
-# First we will declare the new class of Bike, which is like a blue print for stamping out objects.
-class Bike
-    #anNndDd theNnn... we set the shared variables for the data we want to collect/create for each of the instances of this Bike class.
-    def initialize(model, wheels = 2, current_speed = 0)
-      #We'll tie all their attributes up with @ so we can call upon them 
-      @model = model
-      @wheels = wheels
-      @current_speed = current_speed
-    end
-    # It then asks me to create a method called 'bike_info' that returns a sentence with all the data we initially called for a new object.
-    # We'll name the method 'get_info'.
-    def get_info
-        # since we've already got the attributes/info tied up top and ready. we can just drop them into a sentence as an output. 
-        "The #{@model} has #{@wheels} wheels and is going #{@current_speed} mph."
-    end
-    # -------------------3b) Add the ability to pedal faster and brake. The pedal_faster method should increase the speed by a given amount.
-     # So we'll be legit with it and name the method 'pedal_faster' and it takes in a variable number.
-    def pedal_faster(num)
-        # everytime it's called upon, we'll add the passed in number to the current speed.
-        @current_speed = @current_speed + num
-    end
-    # The brake method should decrease the speed by a given amount. The bike cannot go negative speeds.
-    def brake(num)
-        # We have to consider we can't go negative speed when braking, so we'll do an if/else conditionals for this method.
-        # we'll set a condtional if statement for how it should respond if we have a negative outcome... in this case, we will assign it to a base of 0.
-        if (@current_speed - num).negative?()
-             @current_speed = 0
-        # the conditional else should function as usual for braking, it should take in the current speed and subtract the given number variable.
-        else @current_speed = @current_speed - num
-        end
-    end
-
-end
-
-# Here we'll create a new instance/object out of the model class we just created up top...
-# We'll call it trek_bike because we're smart like that. and set the Bike to .new and pass in the its name as the first and only rgument 'Trek bike' because that is all we need. 
-trek_bike = Bike.new('Trek bike')
-# lets call upon the method we built inside the new instance/object of the bike.
-p trek_bike.get_info
-# Output: "The Trek bike has 2 wheels and is going 0 mph."
 # Expected output example: 'The Trek bike has 2 wheels and is going 0 mph.'
-# Just to see what would happenm I played around by adding random values onto the new bike.
-# new_model = Bike.new('Trek bike', 0, 4)
-# "The Trek bike has 0 wheels and is going 4 mph."
-# Obviously this bike is flying... 
+
 
 
 # -------------------3b) Add the ability to pedal faster and brake. The pedal_faster method should increase the speed by a given amount. The brake method should decrease the speed by a given amount. The bike cannot go negative speeds.
 
-trek_bike.pedal_faster(10) 
-# => 10
-trek_bike.pedal_faster(18) 
-# => 28
-trek_bike.brake(5) 
-# => 23
-trek_bike.brake(25) 
-# => 0
-
-p trek_bike.get_info
-# "The Trek bike has 2 wheels and is going 0 mph."
+# Expected output example: my_bike.pedal_faster(10) => 10
+# Expected output example: my_bike.pedal_faster(18) => 28
+# Expected output example: my_bike.brake(5) => 23
+# Expected output example: my_bike.brake(25) => 0
